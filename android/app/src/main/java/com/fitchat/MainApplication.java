8f98a84b8bd154d48eec7400bd574f280c581be2
bepicard <benjamin.picard@etu.uca.fr>
2019-05-15 13:37:28 +0100

    dialog with the bot works

--- android/app/src/main/java/com/fitchat/MainApplication.java
+++ android/app/src/main/java/com/fitchat/MainApplication.java
@@ -3,6 +3,7 @@ package com.fitchat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
 import com.wenkesj.voice.VoicePackage;
 import de.innfactory.apiai.RNApiAiPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new TextToSpeechPackage(),
             new VoicePackage(),
             new RNApiAiPackage(),
             new VectorIconsPackage(),
--- src/screens/VoiceBot/index.js
+++ src/screens/VoiceBot/index.js
@@ -11,6 +11,8 @@ import { GiftedChat } from 'react-native-gifted-chat';
 import Dialogflow, { Dialogflow_V2 } from 'react-native-dialogflow';
 import auth from './auth.json';
 
+import Tts from 'react-native-tts';
+
 const COACH = {
   _id: 2,
   name: "React Native",
@@ -18,34 +20,50 @@ const COACH = {
 }
 
 export default class VoiceBot extends Component {
-  state = {
-    messages: [{
-      _id: 1,
-      text: "Hello developer",
-      createdAt: new Date(),
-      user: COACH
-    }]
-  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [{
+        _id: 1,
+        text: "Hello, I'am your coach bot. Say something!",
+        createdAt: new Date(),
+        user: COACH
+      }],
+      isListenerActive: false
+    };
+  }
+
 
   componentDidMount() {
-    Dialogflow.setConfiguration(
+    Dialogflow.setConfiguration( //V1 configuration (for "Listening")
       "753f8f35601f48a6a558e80cc4b2edde",
       Dialogflow.LANG_ENGLISH,
     );
 
-    Dialogflow_V2.setConfiguration(
+    /*Dialogflow_V2.setConfiguration( //V2 configuration (for text query)
       auth.client_email,
       auth.private_key,
       Dialogflow_V2.LANG_ENGLISH,
       auth.project_id
-    );
-    Dialogflow_V2.onListeningStarted(() => console.log("listening started"));
+    );*/
+
+    /*Dialogflow_V2.onListeningStarted(() => console.log("listening started")); //listeners (for android)
     Dialogflow_V2.onListeningCanceled(() => console.log("listening canceled"));
     Dialogflow_V2.onListeningFinished(() => console.log("listening finished"));
-    Dialogflow_V2.onAudioLevel(level => console.log(level));
+    Dialogflow_V2.onAudioLevel(level => console.log(level));*/
+
+    /*Dialogflow.onListeningStarted(() => console.log("listening started")); //listeners (for android)
+    Dialogflow.onListeningCanceled(() => console.log("listening canceled"));
+    Dialogflow.onListeningFinished(() => console.log("listening finished"));
+    Dialogflow.onAudioLevel(level => console.log(level));*/
+
+    Tts.addEventListener('tts-finish', (event) => { //start a new Listening when speech end
+      this._startListening()
+    });
   }
 
-  _sendBotMessage(text) {
+  _sendBotMessage(text) { //send a bot response
     let msg = {
       _id: this.state.messages.length + 1,
       text,
@@ -58,7 +76,7 @@ export default class VoiceBot extends Component {
     }));
   }
 
-  _sendUserMessage(text) {
+  _sendUserMessage(text) { //send a user message (when "Listening")
     let msg = {
       _id: this.state.messages.length + 1,
       text,
@@ -71,40 +89,64 @@ export default class VoiceBot extends Component {
     }));
   }
 
-  _onSend(messages = []) {
+  _speak(text) {
+    Tts.getInitStatus().then(() => {
+      Tts.speak(text);
+    });
+  }
+
+  _onSend(messages = []) { //when user type and send a message
     console.log(messages)
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages)
     }));
 
     let message = messages[0].text;
-    Dialogflow_V2.requestQuery(
+    Dialogflow.requestQuery(
       message,
-      result => this._sendBotMessage(result.queryResult.fulfillmentMessages[0].text.text[0]),
+      result => {
+        this._sendBotMessage(result.result.fulfillment.speech)
+        this._speak(result.result.fulfillment.speech)
+        /*this._sendBotMessage(result.queryResult.fulfillmentMessages[0].text.text[0])
+        this._speak(result.queryResult.fulfillmentMessages[0].text.text[0])*/
+        //Dialogflow.subscription.remove();
+      },
       error => console.log(error)
     );
   }
 
-  _renderActions(props) {
+  _startListening() { //start the "Listening" action
+    if(this.state.isListenerActive) {
+      Dialogflow.finishListening()
+      Dialogflow.subscription.remove();
+    }
+    this.setState({isListenerActive: true})
+    Dialogflow.startListening(
+      result => {
+        this._sendUserMessage(result.result.resolvedQuery)
+        this._sendBotMessage(result.result.fulfillment.speech)
+        this._speak(result.result.fulfillment.speech)
+        Dialogflow.subscription.remove();
+        this.setState({isListenerActive: false})
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  _renderActions(props) { //mic button render
     return (
       <Button
         onPress={() => {
-          Dialogflow.startListening(
-            result => {
-              console.log(result)
-              props.context._sendUserMessage(result.result.resolvedQuery)
-              props.context._sendBotMessage(result.result.fulfillment.speech)
-              Dialogflow.subscription.remove(); 
-            },
-            error => console.log(error)
-          )
+          props.context._startListening()
         }}
         transparent>
         <Icon name="microphone" size={30} color={"black"} style={{marginLeft: 10}}/>
       </Button>
     );
   }
-//props.sendBotResponse(result.result.fulfillment.speech)
+
   render() {
     return (
       <Container>
