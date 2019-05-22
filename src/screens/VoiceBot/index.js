d95553dbaa5e995e3dbffb11dbe24ad656b661b7
bepicard <benjamin.picard@etu.uca.fr>
2019-05-22 12:06:55 +0100

    Voice button update

--- src/screens/VoiceBot/index.js
+++ src/screens/VoiceBot/index.js
@@ -34,7 +34,7 @@ export default class VoiceBot extends Component {
         user: COACH
       }],
       isMicOn: false,
-      lastMsgIsText: false
+      isSpeaking: false
     };
   }
 
@@ -47,6 +47,7 @@ export default class VoiceBot extends Component {
     );
 
     Tts.addEventListener('tts-finish', (event) => { //start a new Listening when speech end
+      this.setState({isSpeaking: false})
       this._startListening()
     });
   }
@@ -78,40 +79,26 @@ export default class VoiceBot extends Component {
   }
 
   _speak(text) {
-    if(!this.state.isMicOn && !this.state.lastMsgIsText) {
+    if(!this.state.isMicOn) {
       Tts.getInitStatus().then(() => {
+        this._stopListening()
+        this.setState({isSpeaking: true, isMicOn: false})
         Tts.speak(text);
       });
     }
   }
 
-  _onSend(messages = []) { //when user type and send a message
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-      lastMsgIsText: true
-    }));
-
-    let message = messages[0].text;
-    Dialogflow_V2.requestQuery(
-      message,
-      result => {
-        this._sendBotMessage(result.queryResult.fulfillmentText)
-        this._speak(result.queryResult.fulfillmentText)
-      },
-      error => console.log(error)
-    );
-  }
 
   _startListening() { //start the "Listening" action
-    Voice.onSpeechStart = () => this.setState({isMicOn: true, lastMsgIsText: false})
+    Voice.onSpeechStart = () => this.setState({isMicOn: true})
     Voice.onSpeechEnd = () => this.setState({isMicOn: false})
     Voice.onSpeechError = (err) => {
       this.setState({isMicOn: false})
       switch(err.error.message.split('/')[0]){ // 6 > No speech input, 7 > No match
         case '6':
           Toast.show({
-            text: "No speech input, try to speak a little louder and close to the microphone",
-            duration: 5000,
+            text: "Listening canceled, no speech input",
+            duration: 2000,
             position: 'top',
             buttonText: "Ok",
             buttonStyle: { backgroundColor: "#5cb85c" }
@@ -119,8 +106,8 @@ export default class VoiceBot extends Component {
           break
         case '7':
           Toast.show({
-            text: "No match, try to speak a little more clearly and slowly",
-            duration: 5000,
+            text: "No match, try to speak more clearly",
+            duration: 4000,
             position: 'top',
             buttonText: "Ok",
             buttonStyle: { backgroundColor: "#5cb85c" }
@@ -136,39 +123,52 @@ export default class VoiceBot extends Component {
       this._sendUserMessage(speech)
       Dialogflow_V2.requestQuery(
         speech,
-        result => {
-          this._sendBotMessage(result.queryResult.fulfillmentText)
-          this._speak(result.queryResult.fulfillmentText)
+        async result => {
+          let results = result.queryResult.fulfillmentMessages.map(item => item.text.text[0])
+          if(results.length == 1) {
+            this._speak(results[0])
+          }
+          for(i = 0; i < results.length; i++) {
+            this._sendBotMessage(results[i])
+            await new Promise((resolve) => setTimeout(() => resolve(), 2000))
+          }
         },
         error => console.log(error)
       );
     }
   }
 
-  _renderActions(props) { //mic button render
+  _stopListening() {
+    Voice.stop((res)=> console.log(res))
+  }
+
+  _renderInputToolbar(props) {
     return (
       <Button
+        style={{alignSelf: 'center'}}
         onPress={() => {
-          if(!props.context.state.isMicOn) {
+          if(!props.context.state.isMicOn && !props.context.state.isSpeaking) {
             props.context._startListening()
+          } else {
+            props.context._stopListening()
           }
         }}
         transparent>
-        <Icon name="microphone" size={30} color={props.context.state.isMicOn ? 'red' : 'black'} style={{marginLeft: 10}}/>
+        <Icon name="microphone" size={40} color={props.context.state.isMicOn ? 'red' : 'black'} style={{marginBottom: 15}}/>
       </Button>
-    );
+    )
   }
 
   render() {
     return (
       <Container>
-        <HeaderBar title='Voice Bot'/>
+        <HeaderBar title='Voice Bot' onLeftButton={ () => console.log("leftButton") }/>
         <Root>
           <GiftedChat
             messages={this.state.messages}
             onSend={messages => this._onSend(messages)}
             user={{ _id: 1 }}
-            renderActions={this._renderActions}
+            renderInputToolbar={this._renderInputToolbar}
             context={this}
           />
         </Root>
